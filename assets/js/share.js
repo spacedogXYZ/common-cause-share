bbd7f6eebe3602a1098e910bcd60ea2613b3511e
Josh Levinger <josh@levinger.net>
2020-09-08 16:28:26 -0700

    push share event to dataLayer

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,10 +1,17 @@
-function PostShare(event, delay) {
+function PostShare(event, delay, method) {
   if (!delay) {  
     delay = 500;
   }
   setTimeout(function(){
       MicroModal.show('postshare')
     }, delay);
+
+  if(method && window.dataLayer) {
+    window.dataLayer.push({
+      event: 'share',
+      method: method,
+     })
+  }
   return true;
 }
 
@@ -13,13 +20,28 @@ function FBMessage(url) {
     method: 'send',
     link: url,
   }, PostShare);
+
+  if(window.dataLayer) {
+    window.dataLayer.push({
+      event: 'share',
+      method: 'messenger',
+    });
+  }
 }
 
 function FBShare(url) {
+
   FB.ui({
     method: 'share',
     href: url,
   }, PostShare);
+
+  if(window.dataLayer) {
+    window.dataLayer.push({
+      event: 'share',
+      method: 'facebook',
+    });
+  }
 }
 
 MicroModal.init();
